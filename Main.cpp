a46ee06f9d82b10292ea4b9d9e7214d0e85a9495
PhamTrungHieu2001 <73734497+PhamTrungHieu2001@users.noreply.github.com>
2022-10-07 23:14:57 +0700

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int code;
+    cin >> code;
+    if (code>=100 && code<=200) {
+        cout << "Subject found!";
+    }
+    else cout << "Invalid!";
+
+    return 0;
+}
