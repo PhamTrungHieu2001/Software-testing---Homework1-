18e8497c59fc78b4b99d988a861b766a23f5eb51
PhamTrungHieu2001 <73734497+PhamTrungHieu2001@users.noreply.github.com>
2022-10-17 22:24:59 +0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 using namespace std;
 
+void check(int code) {
+    if (code>=100 && code<=200) 
+        cout << "Subject found!";
+    else 
+        cout << "Invalid!";
+}
+
 int main() {
     int code;
     cin >> code;
-    if (code>=100 && code<=200) {
-        cout << "Subject found!";
-    }
-    else cout << "Invalid!";
-
-    return 0;
+    check(code);
 }
